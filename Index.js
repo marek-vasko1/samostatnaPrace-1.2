dd8756df204c107fa8b39abcc62742db2dd0466d
Marek <72089849+marek-vasko@users.noreply.github.com>
2021-10-04 17:04:38 +0200

    first commit

---
+++ Index.js
@@ -0,0 +1,19 @@
+
+for(i = 0; i < 1; i++){
+    let pole =["Lodash", "React","Next","Strapi", "Axios", "Typescript"]
+    console.log(pole);
+    pole.reverse();
+    console.log(pole);
+    pole.sort();
+    console.log(pole);
+}
+/*const car ={
+Brand="blbla",
+Model="xy",
+Year= "1998",
+}
+const car = new newcar(car);
+console.log(car)*/
+
+const random = Math.floor(Math.random() * 555);
+console.log(random);
